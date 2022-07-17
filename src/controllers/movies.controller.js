a8418fc9e4b6056ce2e5b7888d729802b1e39083
Geronimo Anselmo <geronimoalejandroanselmo@gmail.com>
2022-07-16 23:22:53 -0300

    Completed movies

--- src/controllers/movies.controller.js
+++
@@ -1,7 +0,0 @@
-const getMovies = (req, res) => {
-
-    
-    return res.status(200).json("value")
-}
-
-module.exports = {getMovies}
--- src/models/movie.js
+++ src/models/movie.js
@@ -6,23 +6,77 @@ const {
 module.exports = (sequelize, DataTypes) => {
   class Movie extends Model {
 
+    toJSON() {
+      return { ...this.get(), genre_uuid: undefined, createdAt: undefined, updatedAt: undefined }
+    }
     static associate({ Genre }) {
       this.belongsTo(Genre,
         {
+
           foreignKey: {
             name: 'genre_uuid',
-            type: DataTypes.UUID, allowNull: false, defaultValue: DataTypes.UUIDV4, unique: true
+            type: DataTypes.UUID, allowNull: false, defaultValue: DataTypes.UUIDV4
           }
+
         })
     }
   }
   Movie.init({
-    uuid: { primaryKey: true, type: DataTypes.UUID, allowNull: false, defaultValue: DataTypes.UUIDV4, unique: true },
-    imgUrl: { type: DataTypes.STRING, allowNull: false },
-    title: { type: DataTypes.STRING, allowNull: false },
-    creationDate: { type: DataTypes.DATE, allowNull: false },
-    genre_uuid: {   type: DataTypes.UUID, allowNull: false, defaultValue: DataTypes.UUIDV4, unique: true },
-    rating: { type: DataTypes.INTEGER, allowNull: false }
+    uuid: {
+      primaryKey: true,
+      type: DataTypes.UUID,
+      allowNull: false,
+      defaultValue: DataTypes.UUIDV4,
+      unique: true
+    },
+    imgUrl: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: true,
+        notEmpty: true,
+        isUrl: true
+      }
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: true,
+        notEmpty: true,
+        isAlphanumeric:true,
+        len:[3,255]
+      }
+    },
+    creationDate: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      validate: {
+        notNull: true,
+        isDate:true,
+      }
+    },
+    genre_uuid: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      defaultValue: DataTypes.UUIDV4,
+      validate:{
+        notNull:true,
+        notEmpty:true,
+      }
+    },
+    rating: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate:{
+        isNumeric:true,
+        isOutRange(value) {
+          if ( value<0 || value>6) {
+            throw new Error('Only 1 to 5 values are allowed');
+          }
+        }
+      }
+    }
   }, {
     sequelize,
     tableName: 'movies',
