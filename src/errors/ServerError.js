cb737043d5bd3bdddfcb67d40fff7803fd5aa449
Geronimo Anselmo <geronimoalejandroanselmo@gmail.com>
2022-07-29 01:40:58 -0300

    Added last fixes

--- src/errors/ServerError.js
+++ src/errors/ServerError.js
@@ -3,7 +3,7 @@ const CustomApiError = require("./CustomApiError");
 class ServerError extends CustomApiError {
 
     handleError(res) {
-        return res.status(500).json(this.error)
+        return res.status(500).json({errors:[this.error]})
     }
 }
 
